2802dc93cfeb7a3fc53c3f770356bf399eb9fbb2
novneetnov <novneet346iith@gmail.com>
2017-01-02 17:08:18 +0530

    Precompile assets with fancybox images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,12 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bower_componenets")
+#Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bower_componenets")
 
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-#Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/bootstrap-touchspin/src/jquery.bootstrap-touchspin.css']
-#Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/fancybox/source/jquery.fancybox.css']
-#Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/owl.carousel/dist/assets/owl.carousel.css']
+Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/bootstrap-touchspin/src/jquery.bootstrap-touchspin.css']
+Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/fancybox/source/jquery.fancybox.css']
+Rails.application.config.assets.precompile += [Rails.root.to_s + '/vendor/assets/bower_components/owl.carousel/dist/assets/owl.carousel.css']
 #Rails.application.config.assets.precompile += %w(*.css *.scss)
