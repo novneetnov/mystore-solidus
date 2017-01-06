01972d259fdbde1dda543af63ee0a1e4ac23ea01
novneetnov <novneet346iith@gmail.com>
2017-01-06 17:28:17 +0530

    Fix the fastview bootstrap touchspin and zoom work

--- app/assets/javascripts/corporate/scripts/layout.js
+++ app/assets/javascripts/corporate/scripts/layout.js
@@ -215,11 +215,11 @@ var Layout = function () {
         
         jQuery(".fancybox-fast-view").fancybox({
 					//type: 'ajax',
-					maxWidth	: 800,
-					maxHeight	: 600,
-					fitToView	: false,
-					width		: '70%',
-					height		: '70%',
+					//maxWidth	: 800,
+					//maxHeight	: 600,
+					fitToView	: true,
+					//width		: '70%',
+					//height		: '70%',
 					autoSize	: false,
 					closeClick	: false,
 					openEffect	: 'none',
